14f4496b03ccc386874ab05c5e252a732bccd7ea
record0001 <61031123+record0001@users.noreply.github.com>
2026-07-04 00:52:58 +0200

    Update test message inside AuthProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { AuthProvider } from './auth/AuthProvider.jsx';
-import { AppRouter } from './router/AppRouter.jsx';
 
 export default function App() {
   return (
     <AuthProvider>
-      <div>TEST INSIDE PROVIDER</div>
+      <h1>AUTH PROVIDER TEST</h1>
     </AuthProvider>
   );
 }
-
