917c246440063717105dd2b6ecfe40de717734a1
record0001 <61031123+record0001@users.noreply.github.com>
2026-07-04 00:50:32 +0200

    Clean up unused code in App.jsx
    Removed commented-out import statements and App component.

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,3 @@ export default function App() {
   );
 }
 
-/*import { AuthProvider } from './auth/AuthProvider.jsx';
-import { AppRouter } from './router/AppRouter.jsx';
-import './ui/styles.css';
-
-export default function App() {
-  return (
-    <AuthProvider>
-      <AppRouter />
-    </AuthProvider>
-  );
-}
-*/
