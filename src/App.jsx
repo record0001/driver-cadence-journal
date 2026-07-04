f2dad2a38f39119ec6a6edc2f76b0045af59574c
record0001 <61031123+record0001@users.noreply.github.com>
2026-07-04 11:41:27 +0200

    Replace header with AppRouter in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { AuthProvider } from './auth/AuthProvider.jsx';
+import { AppRouter } from './router/AppRouter.jsx';
+import './ui/styles.css';
 
 export default function App() {
   return (
     <AuthProvider>
-      <h1>AUTH PROVIDER TEST</h1>
+      <AppRouter />
     </AuthProvider>
   );
 }
